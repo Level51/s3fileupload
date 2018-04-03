b81d609810a4167ab3f3979fda36606b660c6530
Julian Scheuchenzuber <js@lvl51.de>
2018-04-03 10:32:30 +0200

    implemented img retry load logic if an error occured

--- js/S3UploadField_downloadtemplate.js
+++ js/S3UploadField_downloadtemplate.js
@@ -1,9 +1,20 @@
-window.tmpl.cache['ss-s3uploadfield-downloadtemplate'] = tmpl(
-	'{% for (var i=0, files=o.files, l=files.length, file=files[0]; i<l; file=files[++i]) { %}' +
+/**
+ * Try to reload img every 500ms if there was an error loading it
+ * @param image
+ */
+function imageFailedLoading(image) {
+  setTimeout(() => {
+  	image.src = `${image.src}#${(new Date()).getTime()}`;
+
+    return true;
+  }, 500);
+}
+
+window.tmpl.cache['ss-s3uploadfield-downloadtemplate'] = tmpl(`${'{% for (var i=0, files=o.files, l=files.length, file=files[0]; i<l; file=files[++i]) { %}' +
 		'<li class="ss-uploadfield-item template-download{% if (file.error) { %} ui-state-error{% } %}" data-fileid="{%=file.id%}">' +
 			'{% if (file.thumbnail_url) { %}' +
 				'<div class="ss-uploadfield-item-preview preview"><span>' +
-					'<img src="{%=file.thumbnail_url%}" alt="" />' +
+					'<img src="{%=file.thumbnail_url%}" alt="" onerror="imageFailedLoading(this)" />' +
 				'</span></div>' +
 			'{% } %}' +
 			'<div class="ss-uploadfield-item-info">' +
@@ -14,7 +25,7 @@ window.tmpl.cache['ss-s3uploadfield-downloadtemplate'] = tmpl(
 					'<span class="name" title="{%=file.name%}">{%=file.name%}</span> ' +
 					'<span class="size">{%=o.formatFileSize(file.size)%}</span>' +
 					'{% if (!file.error) { %}' +
-						'<div class="ss-uploadfield-item-status ui-state-success-text" title="'+ss.i18n._t('UploadField.Uploaded', 'Uploaded')+'">'+ss.i18n._t('UploadField.Uploaded', 'Uploaded')+'</div>' +
+						'<div class="ss-uploadfield-item-status ui-state-success-text" title="'}${ss.i18n._t('UploadField.Uploaded', 'Uploaded')}">${ss.i18n._t('UploadField.Uploaded', 'Uploaded')}</div>` +
 					'{% } else {  %}' +
 						'<div class="ss-uploadfield-item-status ui-state-error-text" title="{%=o.options.errorMessages[file.error] || file.error%}">{%=o.options.errorMessages[file.error] || file.error%} test</div>' +
 					'{% } %}' +
@@ -22,7 +33,7 @@ window.tmpl.cache['ss-s3uploadfield-downloadtemplate'] = tmpl(
 				'</label>' +
 				'{% if (file.error) { %}' +
 					'<div class="ss-uploadfield-item-actions">' +
-						'<div class="ss-uploadfield-item-cancel ss-uploadfield-item-cancelfailed delete"><button class="icon icon-16" data-icon="delete" title="' + ss.i18n._t('UploadField.CANCELREMOVE', 'Cancel/Remove') + '">' + ss.i18n._t('UploadField.CANCELREMOVE', 'Cancel/Remove') + '</button></div>' +
+						`<div class="ss-uploadfield-item-cancel ss-uploadfield-item-cancelfailed delete"><button class="icon icon-16" data-icon="delete" title="${ss.i18n._t('UploadField.CANCELREMOVE', 'Cancel/Remove')}">${ss.i18n._t('UploadField.CANCELREMOVE', 'Cancel/Remove')}</button></div>` +
 					'</div>' +
 				'{% } else { %}' +
 					'<div class="ss-uploadfield-item-actions">{% print(file.buttons, true); %}</div>' +
@@ -32,5 +43,4 @@ window.tmpl.cache['ss-s3uploadfield-downloadtemplate'] = tmpl(
 				'<div class="ss-uploadfield-item-editform"><iframe frameborder="0" data-src="{%=file.edit_url%}" src="about:blank"></iframe></div>' +
 			'{% } %}' +
 		'</li>' +
-	'{% } %}'
-);
+	'{% } %}');
